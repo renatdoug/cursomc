268ea3fa6d1347f71d8a268b046ce31443b09c0b
Renato Douglas <renatdoug@hotmail.com>
2024-06-07 17:36:30 -0300

    Criando Repository e Service para Categoria

---
+++ src/main/java/com/renatdoug/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package com.renatdoug.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.renatdoug.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+    
+}
